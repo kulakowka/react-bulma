bc4cabeb68789e38238c17a1c2ca5272935ad2be
Julian Böhnke <jboehnke@weluse.de>
2017-10-10 18:14:02 +0200

    remove not used props

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -4,26 +4,6 @@ import classNames from 'classnames'
 
 const Link = ({
   as: A,
-  black,
-  danger,
-  dark,
-  info,
-  large,
-  light,
-  link,
-  medium,
-  fullwidth,
-  outlined,
-  primary,
-  small,
-  success,
-  warning,
-  white,
-  inverted,
-  hovered,
-  focused,
-  active,
-  loading,
   className,
   ...props
 }) => {
@@ -36,27 +16,6 @@ Link.displayName = 'Link'
 
 Link.propTypes = {
   as: PropTypes.node,
-  black: PropTypes.bool,
-  danger: PropTypes.bool,
-  dark: PropTypes.bool,
-  info: PropTypes.bool,
-  fullwidth: PropTypes.bool,
-  large: PropTypes.bool,
-  light: PropTypes.bool,
-  link: PropTypes.bool,
-  medium: PropTypes.bool,
-  outlined: PropTypes.bool,
-  primary: PropTypes.bool,
-  small: PropTypes.bool,
-  success: PropTypes.bool,
-  warning: PropTypes.bool,
-  white: PropTypes.bool,
-  inverted: PropTypes.bool,
-  hovered: PropTypes.bool,
-  focused: PropTypes.bool,
-  active: PropTypes.bool,
-  loading: PropTypes.bool,
-  children: PropTypes.node
 }
 
 Link.defaultProps = {
